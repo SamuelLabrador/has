7f7c480189fe755a02ff1d08b51e66801ec0e37c
SamuelLabrador <sam.labrador@Yahoo.com>
2020-04-16 23:51:11 -0700

    Cleaning up the project dir. Prepping for kubernetes deployment

--- has_tf_server/save_model.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-import tensorflow_hub as hub
-
-url = "https://tfhub.dev/google/faster_rcnn/openimages_v4/inception_resnet_v2/1"
-detector = hub.KerasLayer(url)
-
-tf.saved_model.save(detector, "/tmp/faster_rcnn/1/")
